916f74269f8c17c5107c19025be42804b850e5f2
asasdll <lollipopwar-love@hotmail.co.th>
2020-09-10 15:14:48 +0700

    up_text

--- resources/views/hr/pdf_leave.blade.php
+++ resources/views/hr/pdf_leave.blade.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+<title>leaveth</title>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('/') }}/assets/img/icon_i2.png" />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
